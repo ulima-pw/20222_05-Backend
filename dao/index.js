0bbf837d831d8e3fd42f4d9c3acd58267a3e9458
ProfN406 <ProfN406@lab.aca>
2022-10-31 12:35:23 -0500

    Creando entidades ciclo y evaluacion asi como sus relaciones

--- dao/index.js
+++ dao/index.js
@@ -41,6 +41,43 @@ const Curso = sequelize.define("curso", {
     freezeTableName : true
 })
 
+const Evaluacion = sequelize.define("evaluacion", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+    },
+    fecha_registro : {
+        type : DataTypes.DATE,
+        allowNull : true
+    },
+    curso_id : {
+        type : DataTypes.UUID,
+        allowNull : false
+    },
+    ciclo_id : {
+        type : DataTypes.UUID,
+        allowNull : false
+    }
+})
+
+const Ciclo = sequelize.define("ciclo", {
+    id : {
+        primaryKey : true,
+        type : DataTypes.UUID,
+        defaultValue : Sequelize.UUIDV4
+    },
+    nombre : {
+        type : DataTypes.STRING(200),
+        allowNull : false
+    }
+})
+
+
 // Relaciones
 // Curso * <----> 1 Carrera
 Curso.belongsTo(Carrera, {
@@ -50,7 +87,21 @@ Carrera.hasMany(Curso, {
     foreignKey : "id"
 })
 
+// Evaluacion * <----> 1 Curso
+Evaluacion.belongsTo(Curso, {
+    foreignKey : "curso_id"
+})
+Curso.hasMany(Evaluacion, {
+    foreignKey : "id"
+})
 
+// Evaluacion * <----> 1 Ciclo
+Evaluacion.belongsTo(Ciclo, {
+    foreignKey : "ciclo_id"
+})
+Ciclo.hasMany(Evaluacion, {
+    foreignKey : "id"
+})
 
 module.exports = {
     Carrera, Curso
